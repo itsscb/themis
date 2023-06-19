0bfa3ae35b90e089a4522f1e5f711d367eec87db
itsscb <dev@itsscb.de>
2023-06-19 21:58:45 +0200

    feat: adds editable columns

--- src/app/tasks-list/tasks-list.component.ts
+++ src/app/tasks-list/tasks-list.component.ts
@@ -11,7 +11,28 @@ export class TasksListComponent {
   constructor(private taskService: TasksService) {}
   tasks: Task[] = []
 
+  clonedTasks: { [s: number]: Task } = {};
+
   ngOnInit(){
     this.tasks = this.taskService.getAll()
   }
+
+  onRowEditInit(task: Task) {
+    this.clonedTasks[task.id as number] = { ...task };
+}
+
+onRowEditSave(task: Task) {
+    // if (task.price > 0) {
+        delete this.clonedTasks[task.id as number];
+        // this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Task is updated' });
+    // } 
+    // else {
+        // this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Invalid Price' });
+    // }
+}
+
+onRowEditCancel(task: Task, index: number) {
+    this.tasks[index] = this.clonedTasks[task.id as number];
+    delete this.clonedTasks[task.id as number];
+}
 }
