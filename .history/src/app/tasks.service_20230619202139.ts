423138927cfc8fa84f9118d5f7b3c17c903213ad
itsscb <dev@itsscb.de>
2023-06-19 21:02:05 +0200

    chore: adds styles of template & updates topbar

---
+++ .history/src/app/tasks.service_20230619202139.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Task } from './shared/task';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TasksService {
+
+  constructor() { }
+
+  getAll(): Task[] {
+    return [
+      {
+        id: 1,
+        timestamp: new Date,
+        begin: new Date('2023-01-06 18:00'),
+        end: new Date('2023-01-06 19:00'),
+        title: 'Test Task 1',
+        category: [{
+          name: 'test'
+        }],
+      },
+      {
+        id: 2,
+        timestamp: new Date,
+        begin: new Date('2023-02-06 18:00'),
+        end: new Date('2023-02-06 19:00'),
+        title: 'Test Task 2',
+        category: [{
+          name: 'test'
+        },{
+          name: 'reocurring'
+        }],
+      },
+    ]
+  }
+}
